fd6409057fefaaf08b55dab8371685169b7b3be5
xjtujack <xjtujack@gmail.com>
2014-08-25 23:48:29 -0500

    update just in case

--- src/inputGeneration/LayoutNode.java
+++ src/inputGeneration/LayoutNode.java
@@ -1,11 +1,51 @@
 package inputGeneration;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 public class LayoutNode {
 
 	private String ID;
 	private String Type;
-	private NamedNodeMap Attributes;
+	private Node Node;
 	
+	public LayoutNode(String type, String id, Node node) {
+		ID = id;
+		Type = type;
+		Node = node;
+	}
+	
+	public String getID() {	
+		return ID;
+	}
+	
+	public String getType() {
+		return Type;
+	}
+ 
+	public NamedNodeMap getAttributes() {
+		return Node.getAttributes();
+	}
+	
+	public boolean hasEventHandler(String EventType) {
+		boolean result = false;
+		Node node = Node.getAttributes().getNamedItem(EventType);
+		if (node!=null)
+			result = true;
+		return result;
+	}
+	
+	public String getEventHandler(String EventType) {
+		String result = "";
+		Node node = Node.getAttributes().getNamedItem(EventType);
+		if (node!=null)
+			result = node.getNodeValue();
+		return result;
+	}
+	
+	public void setEventHandler(String EventType, String methodName) {
+		Element e = (Element) Node;
+		e.setAttribute(EventType, methodName);
+	}
 }
--- src/inputGeneration/StaticInfo.java
+++ src/inputGeneration/StaticInfo.java
@@ -14,6 +14,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import main.Main;
 import main.Paths;
 
 public class StaticInfo {
