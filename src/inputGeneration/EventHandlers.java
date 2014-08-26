1583e5836c51302b23a521d8c78deffbb0278dba
xjtujack <xjtujack@gmail.com>
2014-08-26 14:10:49 -0500

    added startActivity(...) and setContentView(...) method signatures

--- src/inputGeneration/EventHandlers.java
+++ src/inputGeneration/EventHandlers.java
@@ -4,7 +4,31 @@ package inputGeneration;
 public class EventHandlers {
 
 	public static final String[] eventHandlers = {
+		// the name should be in the same format with xml layouts
 		"android:OnClick"
+	};
+	
+	public static final String[] intentSigs = {
+		// must be virtual invoke
+		": void startActivity(android.content.Intent)>",
+		": void startActivity(android.content.Intent,android.os.Bundle)>",
+		": void startActivityForResult(android.content.Intent,int)>",
+		": void startActivityForResult(android.content.Intent,int,android.os.Bundle)>",
+		": void startActivityFromChild(android.app.Activity,android.content.Intent,int)>",
+		": void startActivityFromChild(android.app.Activity,android.content.Intent,int,android.os.Bundle)>",
+		": void startActivityFromFragment(android.app.Fragment,android.content.Intent,int)>",
+		": void startActivityFromFragment(android.app.Fragment,android.content.Intent,int,android.os.Bundle)>",
+		": boolean startActivityIfNeeded(android.content.Intent,int)>",
+		": boolean startActivityIfNeeded(android.content.Intent,int,android.os.Bundle)>",
+		": void startActivities(android.content.Intent[])>",
+		": void startActivities(android.content.Intent[],android.os.Bundle)>"
+	};
+	
+	public static final String[] setContentViewSigs = {
+		// must be virtual invoke
+		": void setContentView(int)>",
+		": void setContentView(android.view.View)>",
+		": void setContentView(android.view.View,android.view.ViewGroup$LayoutParams)>"
 	};
 	
 	public static boolean isEventHandler(String attribute) {
