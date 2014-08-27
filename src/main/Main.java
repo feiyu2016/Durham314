ed83857b6fc304d4543bef765f04806414a221e6
xjtujack <xjtujack@gmail.com>
2014-08-27 00:14:11 -0500

    have to merge Main.java because of conflict..

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,13 @@
 package main;
 
 import inputGeneration.JDBStuff;
-import inputGeneration.SimpleTesting;
 import inputGeneration.StaticInfo;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class Main {
@@ -15,37 +15,12 @@ public class Main {
 	public static ArrayList<Node> nodes = new ArrayList<Node>();
 	
 	public static void main(String[] args) {
-		//File file = new File("/home/wenhaoc/AppStorage/TestThreads.apk");
-		//File file = new File("/home/wenhaoc/workspace/Test/bin/Test.apk");
-
-		//initAnalysis(file);
 		
 		File file = new File("/home/wenhaoc/AppStorage/Fast.apk");
-		StaticInfo.getActivityNames(file);
-		for (Node node: nodes) {
-			Element e = (Element) node;
-			System.out.println(e.getAttributes().getLength());
-			e.setAttribute("LOL", "aaa");
-		}
-		System.out.println("---------------");
-		for (Node node: nodes) {
-			Element e = (Element) node;
-			System.out.println(e.getAttributes().getLength());
-			System.out.println(node.getAttributes().getNamedItem("LOL").getNodeValue());
-			
-		}
+		
 		//initAnalysis(file);
 		try {
-			
-		} catch (Exception e1) {e1.printStackTrace();}
-
-
-//		initAnalysis(file);
-		//testJDB(file);
-
-		
-		try {
-			SimpleTesting.clickAll(file);
+			//SimpleTesting.clickAll(file);
 		} catch (Exception e) {	e.printStackTrace();}
 	}
 	
@@ -65,5 +40,33 @@ public class Main {
 			jdb.setBreakPointLine("com.example.helloworld.MainActivity", 321);
 			while (true) {}
 		} catch (Exception e) {	e.printStackTrace();}
+	}
+	
+	private static void testOnCreateSig() throws Exception{
+		int count = 0;
+		File[] files = new File("/home/wenhaoc/AppStorage/PlayStoreApps/").listFiles();
+		for (File file: files) {
+			System.out.println(file.getName());
+			ArrayList<String> aa = StaticInfo.getActivityNames(file);
+			for (String a: aa) {
+				System.out.println(" " + a);
+				File classFolder = new File(Paths.appDataDir + file.getName() + "/ClassesInfo/" + a);
+				if (!classFolder.exists())	continue;
+				
+				BufferedReader in = new BufferedReader(new FileReader(classFolder + "/ClassInfo.csv"));
+				String line;
+				while ((line = in.readLine())!=null) {
+					if (!line.startsWith("Method,"))	continue;
+					String[] s = line.split(",");
+					if (s[1].equals("onCreate"))
+						if (!s[5].equals("void onCreate(android.os.Bundle)")) {
+							count++;
+							System.out.println("--->  " + s[5]);
+						}
+				}
+				in.close();
+			}
+		}
+		System.out.println(count);
 	}
 }
