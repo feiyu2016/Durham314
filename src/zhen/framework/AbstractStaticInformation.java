18f88f0765265ddb28912953f466e86f15f2be55
sindorie <sin.square6@gmail.com>
2014-09-08 09:27:15 -0500

    delete unnecessary files

--- src/zhen/framework/AbstractStaticInformation.java
+++
@@ -1,9 +0,0 @@
-package zhen.framework;
-
-public abstract class AbstractStaticInformation extends AbstractBaseFrameworkComponent{
-
-	public AbstractStaticInformation(Framework frame) {
-		super(frame);
-	}
-
-}
